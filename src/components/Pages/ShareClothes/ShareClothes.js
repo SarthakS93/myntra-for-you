972e7e9e13ed65f1f8d93eb40d5bcac955e4dca1
Sarthak <sarthak.subramanian@myntra.com>
2021-08-26 16:32:36 +0530

    adds image upload code

--- src/components/Pages/ShareClothes/ShareClothes.js
+++ src/components/Pages/ShareClothes/ShareClothes.js
@@ -3,6 +3,11 @@ import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 
+
+/**
+ * image upload tutorial - https://programmingwithmosh.com/javascript/react-file-upload-proper-server-side-nodejs-easy/
+ */
+
 const returnTypeList = [
     {
         label: "Buyback",
@@ -22,7 +27,8 @@ class ShareClothes extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        returnType: null
+        returnType: null,
+        selectedFile: null,
       };
     }
   
@@ -34,10 +40,21 @@ class ShareClothes extends React.Component {
       
     }
 
-    handleChange = (event) => {
+    onReturnTypeChange = (event) => {
         this.setState({
-            returnType:event.target.value
+            returnType:event.target.value,
         });
+    };
+
+    onImageUpload = (event) => {
+        console.log(event.target.files[0]);
+        this.setState({
+            selectedFile: event.target.files[0],
+        });
+    }
+
+    onSubmit = (event) => {
+        console.log("On Submit");
     };
   
     render() {
@@ -45,17 +62,19 @@ class ShareClothes extends React.Component {
         <div>
             <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <div style={{marginTop: "100px", marginBottom: "100px", height: "500px"}}>
-                    <div style={{marginTop: "15px"}}><TextField id="standard-required" defaultValue="" label="Category" /></div>
-                    <div style={{marginTop: "15px"}}><TextField id="standard-required" defaultValue="" label="Brand" /></div>
-                    <div style={{marginTop: "15px"}}><TextField id="standard-required" defaultValue="" label="Description" /></div>
+                    <input type="file" name="file" onChange={this.onImageUpload} accept="image/*" />  
+                    <div style={{marginTop: "15px"}}><TextField id="standard-required" defaultValue="" label="Category" style={{width: "100%"}} /></div>
+                    <div style={{marginTop: "15px"}}><TextField id="standard-required" defaultValue="" label="Brand" style={{width: "100%"}} /></div>
+                    <div style={{marginTop: "15px"}}><TextField id="standard-required" defaultValue="" label="Description" style={{width: "100%"}} /></div>
                     <div style={{marginTop: "15px"}}>
                         <TextField
                             id="standard-select-currency"
                             select
                             label="Select"
                             value={this.state.returnType}
-                            onChange={this.handleChange}
+                            onChange={this.onReturnTypeChange}
                             helperText="Please select your return type"
+                            style={{width: "100%"}}
                             >
                             {returnTypeList.map((option) => (
                                 <MenuItem key={option.value} value={option.value}>
@@ -65,7 +84,7 @@ class ShareClothes extends React.Component {
                         </TextField>
                     </div>    
                     <div style={{marginTop: "15px"}}>
-                        <Button variant="contained" style={{backgroundColor: "#9ce7d1", padding: "15px", border: "none", width: "100%"}}>Submit</Button>
+                        <Button variant="contained" style={{backgroundColor: "#9ce7d1", padding: "15px", border: "none", width: "100%"}} onClick={this.onSubmit}>Submit</Button>
                     </div>    
                 </div>
             </div>    
